f2bd6454ce65d6625d1b6e7130919a69d806b185
fengmuxia <fengmuxia@git.com>
2025-06-25 13:31:48 +0800

    rm /target文件夹

--- enum_study/src/main.rs
+++ enum_study/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("Hello, world!");
+    enum_example();
 }
 
 #[derive(Debug)]
--- enum_study/target/debug/deps/enum_study-5663e1855723998d.d
+++
@@ -1,5 +0,0 @@
-/root/rust-study/enum_study/target/debug/deps/libenum_study-5663e1855723998d.rmeta: src/main.rs
-
-/root/rust-study/enum_study/target/debug/deps/enum_study-5663e1855723998d.d: src/main.rs
-
-src/main.rs:
